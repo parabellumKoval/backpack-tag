9a2d3063b161aedb0a78e105faacd5bac39e3aab
Андрей <parabellum.koval@gmail.com>
2022-12-18 20:43:13 +0200

    Api routes & controller

--- src/config/reviews.php
+++ src/config/reviews.php
@@ -8,9 +8,19 @@ return [
   // CATALOG
   'per_page' => 12,
 
-  // USER
-  'user_model' => 'Backpack\Profile\app\Models\Profile',
+  // OWNER
+  'owner_model' => 'Backpack\Profile\app\Models\Profile',
 
-  // PRODUCT
-  'product_model' => 'Backpack\Store\app\Models\Product',
+  // Seed batabase
+  'reviewable_model' => 'Backpack\Store\app\Models\Product',
+
+  'rating_type' => 'detailed', // 'detailed' - allow multiple rating params, 'simple' - allow only single digit  
+
+  'detailed_rating_params' => [
+    'param_1' => 'label_1',
+    'param_2' => 'label_2',
+    'param_3' => 'label_2',
+  ],
+
+  'rating_length' => 5
 ];
