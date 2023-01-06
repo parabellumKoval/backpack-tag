082cc8e7661c6141c30278efa971516e2229d306
Андрей <parabellum.koval@gmail.com>
2023-01-06 18:19:34 +0200

    fix guard

--- src/routes/api/review.php
+++ src/routes/api/review.php
@@ -5,22 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use Backpack\Reviews\app\Http\Controllers\Api\ReviewController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-$auth_guard = config('backpack.reviews.auth_guard', 'profile');
+//$auth_guard = config('backpack.reviews.auth_guard', 'profile');
 
 Route::prefix('api/reviews')->controller(ReviewController::class)->group(function () {
   
@@ -28,7 +13,7 @@ Route::prefix('api/reviews')->controller(ReviewController::class)->group(functio
 
   Route::get('/{id}', 'show');
 
-  Route::post('', 'create')->middleware(['api', "auth:${$auth_guard}"]);
+  Route::post('', 'create')->middleware('api');
   Route::post('{id}/like', 'likeOrDislike');
 
 });
