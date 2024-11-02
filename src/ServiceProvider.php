f3a7ee816baa141c1d8c5af804a2a461e68703bc
Андрей <parabellum.koval@gmail.com>
2024-11-02 14:21:10 +0200

    publiches to packages/backpack/tag

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -37,7 +37,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 
 
       $this->publishes([
-          __DIR__.'/public' => public_path('ak_tags'),
+          __DIR__.'/public' => public_path('packages/backpack/tag'),
       ], 'public');
     }
 
