a892cb788ed68d209b03c9954895897d4ba7c9bd
Андрей Коваль <andrejkoval@Andrejs-Mac-mini.local>
2025-01-29 19:42:09 +0200

    and no-tag filter

--- src/app/Traits/TagFields.php
+++ src/app/Traits/TagFields.php
@@ -14,11 +14,17 @@ trait TagFields {
       'type' => 'select2',
     ], function(){
       $tags = Tag::all()->pluck('text', 'id')->toArray();
+      $tags['no'] = 'Без тега';
+
       return $tags;
     }, function($id){
-      $this->crud->query->whereHas('tags', function ($query) use ($id) {
-        $query->where('tag_id', $id);
-      });
+      if($id === 'no') {
+        $this->crud->query->has('tags', '=', 0);
+      }else {
+        $this->crud->query->whereHas('tags', function ($query) use ($id) {
+          $query->where('tag_id', $id);
+        });
+      }
     });
   }
 
